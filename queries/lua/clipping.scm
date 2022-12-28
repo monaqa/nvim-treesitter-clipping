edfe30a4d9110cb50334cb22e37c8ff0d08e3dea
monaqa <cmonaqa@gmail.com>
2022-12-29 08:27:11 +0900

    modify lua query

--- queries/lua/clipping.scm
+++ queries/lua/clipping.scm
@@ -1,19 +1,28 @@
 ;; A query for lua files
 
+; vim.cmd [[
+;   %% HERE %%
+; ]]
 (function_call
   name: (_) @funcname
   arguments: (arguments (string) @clip)
   (#eq? @funcname "vim.cmd")
+  (#match? @clip "^\\[")  ; enable only [[ .. ]] string
   (#set! "filetype" "vim")
   (#offset! @clip 1 0 -1 0)
   )
 
+; some_func [[
+;   %% HERE %%
+; ]]
 (function_call
   name: (_)
   arguments: (arguments (string) @clip)
+  (#match? @clip "^\\[")
   (#offset! @clip 1 0 -1 0)
   )
 
+; comment lines
 (
  (comment) @clip_seq
  (#set! "prefix_pattern" "\\s*---\\?\\s*")
